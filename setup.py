9a31f0b08cfeda746e926aa37f450654bc5d7bc4
Penguin.wp <Whirlpool.Programmer@outlook.com>
2021-07-03 15:17:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
 	name='Proget',
-	version='1.0b4',    
+	version='1.0',    
 	description='''A software installation tool''',
 	long_description=long_description,
 	long_description_content_type="text/markdown",
