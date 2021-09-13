2f9f47e82ac104bdc0bb532ddbaefe58734d2f4e
Penguin.wp <Whirlpool.Programmer@outlook.com>
2021-09-13 17:32:35 +0530

    Update __init__.py

--- proget/__init__.py
+++ proget/__init__.py
@@ -1,7 +1,7 @@
 '''
 made on Python 3.8.10
 
-NOTE:
+PLEASE NOTE:
 This module works for python 3.x only!
 because "urllib" module is not avaliable in Python 2.x
 but in python 2.x you can use this code with "urllib2" module instead.
@@ -10,6 +10,7 @@ How to run in python shell?
 >>> import proget
 >>> proget.main("download my_software(version)") #it downloads "my_software" with version being "version".
 '''
+
 import os
 import sys
 import time
@@ -19,6 +20,8 @@ import tempfile
 import requests
 import platform
 import subprocess
+import animation
+import zipfile
 from github import Github
 import urllib.request as req
 
@@ -32,41 +35,54 @@ def clear():
 		command = 'cls'
 	os.system(command)
 
+def getfile(args):
+	args = list(args)
+	for file in args:
+		num = 0
+		print("Starting to download..",end = "\r")
+		url = file
+		local_filename = file.split("/")[-1]
+		with requests.get(url, stream=True) as r:
+			r.raise_for_status()
+			with open(local_filename, 'wb') as f:
+				for chunk in r.iter_content(chunk_size=8192):
+					f.write(chunk)
+					print("Downloading file '{}' [{}]".format(file,progress[num]), end = "\r")
+					if num != 3: num = num+1 
+					else: num = 0
+		print("\ndownloaded file {}".format(local_filename))
+
+@animation.wait(['pro-Getting the repo (|)','pro-Getting the repo (/)','pro-Getting the repo (-)','pro-Getting the repo (\\)'])
 def githubGet(args):
 	args = list(args)
 	for repo in args:
-		repoName = repo
-		num = 0
-		files = []
-		repo = Github().get_repo(repo)
-		contents = repo.get_contents("")
-		while contents:
-			print("Getting files info [{}]".format(progress[num]), end = "\r")
-			file_content = contents.pop(0)
-			if file_content.type == "dir":
-				contents.extend(repo.get_contents(file_content.path))
-			else:
-				files.append(str(file_content)[18:-2])
-			if num != 3: num = num+1 
-			else: num = 0
-		print("starting to download repository {}".format(repoName), end = "\r")
-		for file in files:
-			if False:
-				continue
-			local_filename = repoName.split("/")[-1]+"/{}".format(file)
-			url = "https://raw.githubusercontent.com/{}/master/{}".format(repoName,file)
-			with requests.get(url, stream=True) as r:
-				r.raise_for_status()
-				if not os.path.exists("/".join(local_filename.split("/")[:-1])):
-					os.makedirs("/".join(local_filename.split("/")[:-1]))
-				with open(local_filename, 'wb') as f:
-					for chunk in r.iter_content(chunk_size=8192):
-						f.write(chunk)
-						print("Downloading.. [{}] file '{}'".format(progress[num], file), end = "\r")
-						if num != 3: num = num+1 
-						else: num = 0
-			print("Finished downloading '{}' from repository '{}'".format(file, repoName))
-
+		repoName = repo.split('/')[1]
+		repoAuthor = repo.split('/')[0]
+		if ':' in repoName:
+			tmprepoName = repoName.split(':')[0]
+			repoMain = repoName.split(':')[1]
+			repoName = tmprepoName
+		else:
+			repoMain = 'main'
+			try:
+				try:
+					tmp = requests.head("https://codeload.github.com/{}/zip/refs/heads/master".format(repo)).headers['Content-Length']
+				except requests.exceptions.HTTPError:
+					print('Repo "{}" does not exist..\nmaybe, you\'ve misspelled it?'.format(repo))
+					sys.exit()
+			except KeyError:
+				repoMain = 'master'
+		url = 'https://codeload.github.com/{}/{}/zip/refs/heads/{}'.format(repoAuthor,repoName,repoMain)
+		local_filename = '{}.temp'.format(repoName)
+		with requests.get(url, stream=True) as r:
+			r.raise_for_status()
+			with open(local_filename, 'wb') as f:
+				for chunk in r.iter_content(chunk_size=8192):
+					f.write(chunk)
+		zipfile.ZipFile(local_filename,'r').extractall("./")
+		os.remove(local_filename)
+		print('Repo: {} download done..'.format(repo))
+					
 
 def download(names):	
 	names = list(names)
@@ -119,7 +135,7 @@ def download(names):
 					if num != 3: num = num+1 
 					else: num = 0
 	
-		print("Finished in {} seconds".format(time.time()-start)+" "*(len(progress[num], url, round(percent,2),int(size)/1024)+2)+"\n")
+		print("Finished in {} seconds".format(time.time()-start), end = "\n")
 		#print("file can be found at {}".format(local_filename))
 		os.system(local_filename)
 		return local_filename
@@ -133,16 +149,37 @@ def main(optional = ""):
 				githubGet(sys.argv[2:])
 			elif "uninstall" in sys.argv[1].lower():
 				print("Uninstaller coming soon..")
+			elif "get" in sys.argv[1].lower():
+				getfile(sys.argv[2:])
 			else:
 				subprocess.check_call(" ".join(sys.argv[1:]))
 		elif len(sys.argv) == 1 or sys.argv[1] in "--help ?":
-			print("use ProGet [option] [argument]")
-			print(" examples:")
-			print("	  proget download software_name(version)")
+			print("""
+proget [option] (arguments)
+
+>> option:
+	download > download a software
+	github   > download github repository
+	get      > download a specific file
+
+>> arguments:
+	names    > the names of files/programs/repositories to download
+
+EXAMPLES:
+	proget download python(v3.8)
+	proget github microsoft/terminal
+	proget get https://www.whirlpool.repl.co/data/sublime_text.yml
+  """)
 	else:
 		command_split = optional.split()
 		if "download" in command_split[1].lower():
 			download(command_split[2:])
+		elif "github" in command_split[1].lower():
+			githubGet(command_split[2:])
+		elif "uninstall" in command_split[1].lower():
+			print("Uninstaller coming soon..")
+		elif "get" in command_split[1].lower():
+			getfile(command_split[2:])
 		else:
 			subprocess.check_call(" ".join(sys.argv[1:]))
 
